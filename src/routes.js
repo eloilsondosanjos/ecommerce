49294f43b125980a4fcd284f93be49ee7448165b
eloilsonRocha <eloilson.ageedu@gmail.com>
2020-05-22 07:50:38 -0300

    criado formulario de cadastro de produtos

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const routes = express.Router();
-
+const express = require("express")
+const routes = express.Router()
+const ProductsController = require ('./app/controllers/ProductsController')
 
 routes.get("/", function (req, res) {
-  return res.render("layout.njk");
-});
+  return res.render("layout.njk")
+})
+
+routes.get("/products/create",ProductsController.create)
+
+
+routes.get("/ads/create", function (req, res) {
+  return res.render("/products/create")
+})
 
 module.exports = routes;
