74071c5344bd6a5bf12f7d827150cf2931323303
eloilsonRocha <eloilson.ageedu@gmail.com>
2020-05-25 07:05:53 -0300

    implementado mask em price para moeda padrão BRL

--- public/scripts.js
+++ public/scripts.js
@@ -1,70 +1,17 @@
-const currentPage = location.pathname;
-const menuItems = document.querySelectorAll('header .links a');
+const Mask = {
+  apply(input,func) {
+    setTimeout(function () {
+        input.value = Mask[func](input.value)
+    }, 1)
 
-for (item of menuItems) {
-  if (currentPage.includes(item.getAttribute('href'))) {
-    item.classList.add('active');
+  },
+  formatBRL(value) {
+    value = value.replace(/\D/g, "")
+
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(value / 100)
   }
 }
 
-// Paginação
-
-function paginate(selectedPage, totalPages) {
-  const pages = [];
-  let oldPage;
-
-  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-    // eslint-disable-next-line eqeqeq
-    const firtsAndLastPage = currentPage == 1 || currentPage == totalPages;
-    const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
-    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
-
-    if (
-      firtsAndLastPage ||
-      (pagesBeforeSelectedPage && pagesAfterSelectedPage)
-    ) {
-      if (oldPage && currentPage - oldPage > 2) {
-        pages.push('...');
-      }
-
-      if (oldPage && currentPage - oldPage == 2) {
-        pages.push(oldPage + 1);
-      }
-      pages.push(currentPage);
-
-      oldPage = currentPage;
-    }
-  }
-
-  return pages;
-}
-
-function createPagination(paginations) {
-    
-const filter = pagination.dataset.filter;
-const page = +pagination.dataset.page;
-const total = +pagination.dataset.total;
-const pages = paginate(page, total);
-
-let elements = '';
-
-for (const page of pages) {
-  if (String(page).includes('...')) {
-    elements += `<span>${page}</span>`;
-  } else {
-    if ( filter ) {
-      elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`;
-    } else {
-      elements += `<a href="?page=${page}">${page}</a>`;
-    }
-  }
-}
-
-pagination.innerHTML = elements;
-}
-
-const pagination = document.querySelector('.pagination');
-
-if(pagination){
-  createPagination(pagination)
-}
