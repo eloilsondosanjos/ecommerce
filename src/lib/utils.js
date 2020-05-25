703daca1b4b77de032774d21bd95b201bb412e98
eloilsonRocha <eloilson.ageedu@gmail.com>
2020-05-25 07:08:09 -0300

    implementado os formularios de cadastro e edição de produtos

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -15,4 +15,11 @@ module.exports = {
       birthDayMonth: `${day}/${month}`,
     };
   },
+
+  formatPrice(price) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price / 100)
+  }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const routes = express.Router()
-const ProductsController = require ('./app/controllers/ProductsController')
+const ProductController = require ('./app/controllers/ProductController')
 
 routes.get("/products", function (req, res) {
   return res.render("layout.njk")
 })
 
-routes.get("/products/create",ProductsController.create)
+routes.get("/products/create", ProductController.create)
+routes.get("/products/:id/edit", ProductController.edit)
 
+routes.post("/products", ProductController.post)
 
 routes.get("/ads/create", function (req, res) {
   return res.render("/products/create")
